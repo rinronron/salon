16e172abaee5ce61491d4efab1f411bb5ed15451
rinronron <87799790+rinronron@users.noreply.github.com>
2022-08-21 22:21:30 +0900

    更新

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 /* ハンバーガーメニュー */
 $(".openbtn").click(function (){
     $(this).toggleClass('active');
-})
+});
 
+$("openbtn").click(function (){
+    $(this).toggleClass('active');
+    $("#g-nav").toggleClass('panelactive');
+});
+
+$("#g-nav a").click(function(){
+    $(".openbtn").removeClass('active');
+    $("#g-nav").removeClass('panelactive');
+});

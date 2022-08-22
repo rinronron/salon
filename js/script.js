d71b9568cc1b2416e0ee9dfd3020ff652af54343
rinronron <87799790+rinronron@users.noreply.github.com>
2022-08-22 21:29:39 +0900

    更新

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 /* ハンバーガーメニュー */
-
 $(".openbtn").click(function (){
     $(this).toggleClass('active');
     $("#g-nav").toggleClass('panelactive');
@@ -8,4 +7,48 @@ $(".openbtn").click(function (){
 $("#g-nav a").click(function(){
     $(".openbtn").removeClass('active');
     $("#g-nav").removeClass('panelactive');
+});
+
+
+/* 一文字ずつ文字を表示 */
+function EachTextAnimeControl() {
+    $('.eachTextAnime').each(function () {
+      var elemPos = $(this).offset().top - 50;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight) {
+        $(this).addClass("appeartext");
+  
+      } else {
+        $(this).removeClass("appeartext");
+      }
+    });
+  }
+
+$(window).scroll(function() {
+    EachTextAnimeControl();
+});
+
+$(window).on('load', function () {
+    var element = $(".eachTextAnime");
+    element.each(function () {
+      var text = $(this).text();
+      var textbox = "";
+      text.split('').forEach(function (t, i) {
+        if (t !== " ") {
+          if (i < 10) {
+            textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
+          } else {
+            var n = i / 10;
+            textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>';
+          }
+  
+        } else {
+          textbox += t;
+        }
+      });
+      $(this).html(textbox);
+    });
+  
+    EachTextAnimeControl();
 });
